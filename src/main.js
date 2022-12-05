65319ed9cf6818cfbca0abac0181c2efa111b6ca
saamcruzzr <contacto.samcruz@gmail.com>
2022-12-05 08:42:35 -0600

    prueba aparecer data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+console.log (data.films[0].poster);
+
+const prueba = document.getElementById("prueba")
+prueba.innerHTML = ` <h1 class= "prueba"> ${data.films[0].title} ${data.films[0].director} </h1> `
